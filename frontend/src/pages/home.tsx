677fa3411fa051f99713400459a9329cf4bee0f2
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-05-26 22:42:51 +0530

    Home page Design added

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import ProductCard from "../components/product-card"
 
 const Home = () => {
+  const addToCartHandler = () => {};
+
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <section>
+        {/* background poster section */}
+      </section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore" >More</Link>
+      </h1>
+
+      <main>
+        <ProductCard productId="12345" name="Macbook" price={400000} stock={15} photo="https://m.media-amazon.com/images/I/710TJuHTMhL._SX522_.jpg" handler={addToCartHandler} />
+      </main>
+
+    </div>
   )
 }
 
